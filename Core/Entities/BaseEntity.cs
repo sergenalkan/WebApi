5ed0095737f7822d4c52d9269ba3b895f92f2c14
Sergen Alkan <sergen_alkan@hotmail.com>
2024-02-26 12:34:56 +0300

    Day 3 Commit 2

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -8,7 +8,6 @@ namespace Core.Entities;
 public class BaseEntity<TId>
 {
     public TId Id { get; set; }
-
     public DateTime CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
     public DateTime? DeletedDate { get; set; }
