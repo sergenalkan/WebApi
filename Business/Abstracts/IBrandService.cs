d02563ce3dcad9fe1044f579ec7cc5994dc05693
Sergen Alkan <sergen_alkan@hotmail.com>
2024-02-26 12:06:37 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts;
+
+public interface IBrandService
+{
+    CreateBrandResponse Add(CreateBrandRequest brand);
+    List<Brand> GetAll();
+}
